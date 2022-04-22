b5c019e75f2ec4cd33ec782422ea530f3842eedd
jm82mexico <jmdelan2012@gmail.com>
2022-04-22 12:19:31 -0500

    Gradientes, ubicaciones y textos

--- Controllers/GraficoController.cs
+++ Controllers/GraficoController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,6 +16,14 @@ namespace CursoGraficos.Controllers
             return View();
         }
 
+        public IActionResult GraficoBarra()
+        {
+            return View();
+        }
+
+        // public string graficoBarras()
+        // {
+        // }
         public string graficoInicial()
         {
             Bitmap oBitmap = new Bitmap(800, 400);
@@ -22,10 +31,41 @@ namespace CursoGraficos.Controllers
             Rectangle rec;
             using (MemoryStream ms = new MemoryStream())
             {
-                rec = new Rectangle(0, 0, 800, 400);
-                grafico.FillRectangle(Brushes.CornflowerBlue, rec);
+                rec = new Rectangle(0, 50, 800, 350);
+                LinearGradientBrush gradiente =
+                    new LinearGradientBrush(rec,
+                        Color.Tomato,
+                        Color.Teal,
+                        LinearGradientMode.BackwardDiagonal);
+
+                grafico.FillRectangle (gradiente, rec);
+                rec = new Rectangle(0, 0, 800, 50);
+                grafico.FillRectangle(Brushes.White, rec);
+
                 rec = new Rectangle(100, 100, 100, 50);
                 grafico.FillRectangle(Brushes.White, rec);
+
+                rec = new Rectangle(600, 100, 100, 50);
+                grafico.FillRectangle(Brushes.White, rec);
+
+                rec = new Rectangle(300, 200, 200, 50);
+                grafico.FillRectangle(Brushes.White, rec);
+
+                rec = new Rectangle(100, 300, 600, 50);
+                grafico.FillRectangle(Brushes.White, rec);
+
+                StringFormat formato =
+                    new StringFormat(StringFormatFlags.NoClip);
+
+                formato.Alignment = StringAlignment.Center;
+
+                grafico
+                    .DrawString("Mi primer gráfico",
+                    new Font("Arial", 14),
+                    Brushes.Black,
+                    new Point(400, 0),
+                    formato);
+
                 oBitmap.Save(ms, ImageFormat.Png);
                 byte[] data = ms.ToArray();
                 return Convert.ToBase64String(data);
