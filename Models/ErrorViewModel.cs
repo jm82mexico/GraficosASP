158e21517ac57f4367c4297dd18e4889159030a5
jm82mexico <jmdelan2012@gmail.com>
2022-04-22 07:15:28 -0500

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CursoGraficos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
